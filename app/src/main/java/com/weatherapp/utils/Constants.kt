49789abf8698db41a644ba77838fab6d36971441
eduardmer <eduardmerkaj1@gmail.com>
2022-10-03 21:18:36 +0200

    apk

--- app/src/main/java/com/weatherapp/utils/Constants.kt
+++ app/src/main/java/com/weatherapp/utils/Constants.kt
@@ -3,4 +3,4 @@ package com.weatherapp.utils
 const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
 const val ICON_BASE_URL = "https://openweathermap.org/img/wn/"
 const val DATE_FORMAT_24H = "HH:mm"
-const val APP_ID = "37f6ca3eb5a94ec5ff7d9600bef088c8"
+const val APP_ID = "37f6ca3eb5a94ecd9600bef088c8"
